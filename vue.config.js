4c86d0e8fec9ae3fe4f45cbd74b7edd9f3a30be6
FlaxidHair <mrkmarlou@gmail.com>
2024-07-29 21:58:00 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+  transpileDependencies: ["vuetify"],
+  publicPath: process.env.NODE_ENV === "production" ? "/test/" : "/",
+});
