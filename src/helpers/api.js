5b0607f660d43aacf4f32e36304b56abda099aec
FlaxidHair <mrkmarlou@gmail.com>
2024-10-23 21:14:42 +0300

    feat: add instance to requests

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://retoolapi.dev/D6xLg4/data',
+  baseURL: 'https://retoolapi.dev/1KJKFH/data',
   headers: {
     'Content-Type': 'application/json',
   },
